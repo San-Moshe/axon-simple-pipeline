2a02867d4085a7b72da4ff584ec0e8157f04c616
sanmo <sanmoshe100@gmail.com>
2023-09-11 13:35:22 +0300

    Multiprocessing queue version

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-import queue
+import multiprocessing
 
 from detector import Detector
 from stream_reader import StreamReader
 from pathlib import Path
-import threading
-
 from video_presentation import Presenter
 
 if __name__ == '__main__':
     file_to_open = Path("D:/people.mp4")
-    read_to_detector_queue = queue.Queue()
-    detector_to_presentation_queue = queue.Queue()
+    read_to_detector_queue = multiprocessing.Queue()
+    detector_to_presentation_queue = multiprocessing.Queue()
 
     reader = StreamReader(file_path=file_to_open, data_dst=read_to_detector_queue)
     detector = Detector(data_src=read_to_detector_queue, data_dst=detector_to_presentation_queue)
     presenter = Presenter(data_src=detector_to_presentation_queue)
 
-    thread_a = threading.Thread(target=reader.start)
-    thread_b = threading.Thread(target=detector.start)
-    thread_c = threading.Thread(target=presenter.start)
+    proc_a = multiprocessing.Process(target=reader.start)
+    proc_b = multiprocessing.Process(target=detector.start)
+    proc_c = multiprocessing.Process(target=presenter.start)
 
-    thread_a.start()
-    thread_b.start()
-    thread_c.start()
+    proc_a.start()
+    proc_b.start()
+    proc_c.start()
+
+    proc_a.join()
+    proc_b.join()
+    proc_c.join()
--- stream_reader.py
+++ stream_reader.py
@@ -1,6 +1,4 @@
 import queue
-import time
-
 import cv2
 
 
